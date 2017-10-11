5e0eea3c58797fac17a39ed0e07645975da1ef26
Alex Howard <thezanke@gmail.com>
2017-10-11 19:32:01 -0400

    randomize case instead of purely alternating

--- src/generate.js
+++ src/generate.js
@@ -1,17 +1,17 @@
 const caption = require('caption');
 const randomstring = require('randomstring');
 
-const alternateCase = str =>
+const randomCase = str =>
   str
     .toLowerCase()
     .split('')
-    .map((char, i) => (i % 2 === 0 ? char : char.toUpperCase()))
+    .map(char => (Math.random() > 0.5 ? char : char.toUpperCase()))
     .join('');
 
 const generate = (text, cb) => {
   console.log(`generating meme for: "${text}"`);
 
-  const mockText = alternateCase(text);
+  const mockText = randomCase(text);
   const words = mockText.split(' ');
   const half = Math.floor(words.length / 2);
   const topCaption = words.slice(0, half).join(' ');
