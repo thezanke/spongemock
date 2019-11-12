e38fedc7a423b3dc22f859d753a90676200672ab
Alex Howard <thezanke@gmail.com>
2019-11-12 12:21:40 -0500

    starting rewrite

--- src/not-found.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-module.exports = (req, res) => {
-  const img = fs.readFileSync(`${__dirname}/img/404bob.jpg`);
-  res.writeHead(200, { 'Content-Type': 'image/gif' });
-  res.end(img, 'binary');
-};
