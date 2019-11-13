ea133ebeaa55652975d5eacd6ef2c38b69145009
Alex Howard <thezanke@gmail.com>
2019-11-13 10:23:07 -0500

    move templates to their own folder

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,25 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { Controller, Get, Param, Res, Logger } from '@nestjs/common';
-
-const BASE_IMAGE_PATH = '/app/images';
+import { ImageService } from './image.service';
 
 @Controller('images')
 export class ImageController {
-  private logger = new Logger('ImageController');
+  constructor(private readonly imageService: ImageService) {}
 
   @Get('/:imageName')
-  getImage(@Param('imageName') imageName, @Res() res) {
-    const imagePath = path.join(BASE_IMAGE_PATH, imageName);
-
-    fs.stat(imagePath, (err) => {
-      if (err) {
-        this.logger.error(err.message);
-        res.sendFile(path.join(BASE_IMAGE_PATH, '404bob.jpg'));
-        return;
-      }
-
-      res.sendFile(imagePath);
-    });
+  async getImage(@Param('imageName') imageName, @Res() res) {
+    const imagePath = await this.imageService.getImageOrFallbackPath(imageName);
+    res.sendFile(imagePath);
   }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,7 +1,12 @@
 import * as randomstring from 'randomstring';
 import * as caption from 'caption';
+import * as path from 'path';
+import * as fs from 'fs';
 import { Injectable, Logger } from '@nestjs/common';
 import { EventEmitter } from 'events';
+
+const BASE_IMAGE_PATH = path.resolve(__dirname, '..', '..', 'images');
+const TEMPLATES_PATH = path.resolve(__dirname, '..', '..', 'templates');
 
 @Injectable()
 export class ImageService extends EventEmitter {
@@ -9,7 +14,7 @@ export class ImageService extends EventEmitter {
 
   randomizeCase(str) {
     return str
-      .toLowerCase()
+    .toLowerCase()
       .split('')
       .map(char => (Math.random() > 0.5 ? char : char.toUpperCase()))
       .join('');
@@ -24,10 +29,12 @@ export class ImageService extends EventEmitter {
     const topCaption = words.slice(0, half).join(' ');
     const bottomCaption = words.slice(half).join(' ');
 
-    const outputFile = `/app/images/mockbob-${randomstring.generate(5)}.jpg`;
+    const outputFile = path.join(BASE_IMAGE_PATH, `mockbob-${randomstring.generate(5)}.jpg`);
 
+    console.log(TEMPLATES_PATH);
+    
     caption.path(
-      `/app/images/template.jpg`,
+      path.join(TEMPLATES_PATH, 'spongemock.jpg'),
       {
         caption: topCaption,
         bottomCaption,
@@ -42,5 +49,20 @@ export class ImageService extends EventEmitter {
         this.emit('generated', responseUrl, userId, imgPath, mockText);
       },
     );
+  }
+
+  getImageOrFallbackPath(imageName) {
+    return new Promise((resolve) => {
+      let imagePath = path.join(BASE_IMAGE_PATH, imageName);
+
+      fs.stat(imagePath, err => {
+        if (err) {
+          this.logger.error(err.message);
+          imagePath = path.join(TEMPLATES_PATH, '404bob.jpg');
+        }
+
+        resolve(imagePath);
+      });
+    });
   }
 }
